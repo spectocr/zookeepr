a5576b4e08ac9f5ba3aa5998c0192f3d524ad410
spectocr <highstakes22@gmail.com>
2021-06-27 13:44:51 -0400

    add Heroku

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+//parse incoming string or array data
+app.use(express.urlencoded({ extended: true}));
+//pars incoming JSON data
+app.use(express.json());
 
 const { animals } = require('./data/animals')
 
@@ -49,6 +55,34 @@ function findById(id, animalsArray) {
     return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray}, null, 2)
+    );
+
+    // return finished code to post route to response
+    return animal;
+}
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species != 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -63,5 +97,20 @@ app.get('/api/animals', (req, res) => {
           res.json(result);
       } else {
           res.send(404);
+      }
+  });
+
+  app.post('/api/animals', (req,res) => {
+      // req.body is where our incoming content will be
+      // set id based on what the next index of the array will be
+      req.body.id = animals.length.toString();
+
+      // if any data in req.body is incorrect, send 400 error back 
+      if (!validateAnimal(req.body)) {
+          res.status(400).send('The animal is no properly formatted.');
+      } else {
+                // add animal to json file an animals array in this function
+      const animal = createNewAnimal(req.body, animals);
+      res.json(req.body);  
       }
   });
